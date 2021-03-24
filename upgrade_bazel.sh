cb4e8d2534d14ef90ff32b6828bc99614e38106c
Jhalak <jhalakp@nvidia.com>
2021-03-23 23:13:01 -0700

    Update tensorflow helper scripts

--- upgrade_bazel.sh
+++ upgrade_bazel.sh
@@ -8,7 +8,10 @@
 # BAZEL_VERSION="1.2.1"
 # BAZEL_VERSION="2.0.0"
 # BAZEL_VERSION="2.1.0"
-BAZEL_VERSION="3.1.0"
+# BAZEL_VERSION="3.1.0"
+BAZEL_VERSION="3.7.2"
+
+PYTHON_BIN_TARGET=$(readlink -f $(which python))
 
 curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
@@ -16,12 +19,12 @@ echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" |
 apt update && apt -y install "bazel-${BAZEL_VERSION}"
 
 rm -f /usr/bin/bazel || true
-rm -f /usr/local/bin/bazel|| true
+rm -f /usr/local/bin/bazel || true
 ln -s "/usr/bin/bazel-${BAZEL_VERSION}" /usr/bin/bazel
 
 echo "=================================================================="
 bazel version
 
 # Restoring Python 3.6 as default
-rm -f /usr/bin/python
-ln -s /usr/bin/python3.6 /usr/bin/python
+#rm -f /usr/bin/python
+#ln -s ${PYTHON_BIN_TARGET} /usr/bin/python
